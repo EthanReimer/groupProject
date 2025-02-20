ba9270825d63c058936710651c118543650072d2
Carl Wessel <c_wessel@fanshaweonline.ca>
2025-02-20 07:24:31 -0500

    Made the basic console app from his jpeg, also made the datamodeler parseJson work(I think) and gave us Tony's old properties folder from last semester to read the data from the sln folder instead of the debug folder so we can access the data folder and its files

--- project1/AdvancedC#Project1/AdvancedC#Project1/DataModeler.cs
+++ project1/AdvancedC#Project1/AdvancedC#Project1/DataModeler.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace AdvancedC_Project1
 {
@@ -15,9 +17,43 @@ namespace AdvancedC_Project1
         }
 
         //Parse JSON method
-        public void ParseJSON(string fileName)
+        public Dictionary<string, CityInfo> ParseJSON(string fileName)
         {
-            Console.WriteLine($"Parsing JSON file: {fileName}");
+            //Read the JSON file
+            string filePath = Path.Combine("data", fileName);
+            string jsonContent = File.ReadAllText(filePath);
+
+            var citiesData = JsonConvert.DeserializeObject<List<dynamic>>(jsonContent);
+
+            //Create a dictionary to hold CityInfo objects
+            Dictionary<string, CityInfo> cityCatalogue = new Dictionary<string, CityInfo>();
+
+            //Populate the dictionary
+            foreach (var cityData in citiesData)
+            {
+                string cityName = cityData.city;
+                string cityAscii = cityData.city_ascii;
+                double? latitudeNull = cityData.lat;
+                double? longitudeNull = cityData.lng;
+                string province = cityData.admin_name;
+                int? populationNull = cityData.population;
+                int? cityIDNull = cityData.id;
+
+                //Gives a default value of 0 if the value is null
+                /*Unhandled exception. Microsoft.CSharp.RuntimeBinder.RuntimeBinderException:
+                Cannot convert null to 'int' because it is a non-nullable value type*/
+                double latitude = latitudeNull ?? 0.0; 
+                double longitude = longitudeNull ?? 0.0;
+                int population = populationNull ?? 0;
+                int cityID = cityIDNull ?? 0; 
+
+                CityInfo cityInfo = new CityInfo(cityID, cityName, cityAscii, population, province, latitude, longitude);
+
+                //Add to the dictionary
+                cityCatalogue[cityName] = cityInfo;
+            }
+
+            return cityCatalogue;
         }
 
         //Parse CSV method
@@ -27,7 +63,7 @@ namespace AdvancedC_Project1
         }
 
         //Delegate for parsing methods
-        public delegate void ParsingMethod(string fileName);
+        public delegate Dictionary<string, CityInfo> ParsingMethod(string fileName);
 
         //Parse file method, switch cases for file types
         public Dictionary<string, CityInfo> ParseFile(string fileName, string fileType)
@@ -37,21 +73,17 @@ namespace AdvancedC_Project1
             switch (fileType.ToLower())
             {
                 case "xml":
-                    parser = ParseXML;
                     break;
                 case "json":
                     parser = ParseJSON;
                     break;
                 case "csv":
-                    parser = ParseCSV;
                     break;
                 default:
                     throw new ArgumentException("Invalid file type.");
             }
 
-            parser(fileName);
-
-            return new Dictionary<string, CityInfo>();
+            return parser(fileName);
         }
     }
 }
--- project1/AdvancedC#Project1/AdvancedC#Project1/Program.cs
+++ project1/AdvancedC#Project1/AdvancedC#Project1/Program.cs
@@ -1,2 +1,66 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using AdvancedC_Project1;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("---------------------");
+        Console.WriteLine("Program functionality");
+        Console.WriteLine("---------------------\n");
+        Console.WriteLine("- To exit program, enter 'exit' at any prompt");
+        Console.WriteLine("- To start program from the begining enter 'restart' at any prompt");
+        Console.WriteLine("- You will be presented with a numbered list of options. Please enter a value, when prompted, to a coresponding file name, file type or data querying routine.");
+        Console.WriteLine("Fetching list of available file names to be processed and queried...\n");
+
+        Console.WriteLine("1) Canadacities-CSV");
+        Console.WriteLine("2) Canadacities-JSON");
+        Console.WriteLine("3) Canadacities-XML");
+
+        Console.WriteLine("\nSelect an option from the list above (e.g. 1, 2)");
+
+        int choice = int.Parse(Console.ReadLine());
+        string fileType = "";
+        string fileName = "";
+
+        switch (choice)
+        {
+            case 1:
+                fileType = "xml";
+                fileName = "Canadacities-XML.xml"; 
+                break;
+            case 2:
+                fileType = "json";
+                fileName = "Canadacities-JSON.json";
+                break;
+            case 3:
+                fileType = "csv";
+                fileName = "Canadacities.csv"; 
+                break;
+            default:
+                Console.WriteLine("Invalid choice.");
+                return;
+        }
+
+        Statistics stats = new Statistics(fileName, fileType);
+        //test to see if it works
+        foreach (var city in stats.CityCatalogue)
+        {
+            CityInfo cityInfo = city.Value; 
+
+            Console.WriteLine($"City Name: {cityInfo.CityName}");
+            Console.WriteLine($"City Ascii: {cityInfo.CityAscii}");
+            Console.WriteLine($"City ID: {cityInfo.CityID}");
+            Console.WriteLine($"Population: {cityInfo.GetPopulation()}");
+            Console.WriteLine($"Province: {cityInfo.GetProvince()}");
+
+            double[] location = cityInfo.GetLocation();
+            Console.WriteLine($"Latitude: {location[0]}");
+            Console.WriteLine($"Longitude: {location[1]}");
+
+        }
+    }
+}
+
+
