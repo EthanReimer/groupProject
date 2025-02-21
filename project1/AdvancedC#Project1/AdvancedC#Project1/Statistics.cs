e2cf2e15062bf2be1eb12e138fc2936c423246ba
Ethan <EthanReimer>
2025-02-21 14:35:44 -0500

    finished the province methods. added: DisplayProvincePopulation DisplayProvinceCities RankProvincesByPopulation RankprovincesByCities GetCapital

--- project1/AdvancedC#Project1/AdvancedC#Project1/Statistics.cs
+++ project1/AdvancedC#Project1/AdvancedC#Project1/Statistics.cs
@@ -195,11 +195,141 @@ namespace AdvancedC_Project1
 
         public void CalculateDistanceBetweenCities(string city1, string city2)
         {
+            //make two variables that find the city in the dictionary and get the location
+            try
+            {
+                CityInfo cityInfo1 = CityCatalogue[city1];
+                CityInfo cityInfo2 = CityCatalogue[city2];
+
+                double lat1 = cityInfo1.GetLocation()[0];
+                double lon1 = cityInfo1.GetLocation()[1];
+
+                double lat2 = cityInfo2.GetLocation()[0];
+                double lon2 = cityInfo2.GetLocation()[1];
+
+                //Haversine formula
+                double R = 6371; // Radius of the earth in km
+
+                double dLat = (lat2 - lat1) * (Math.PI / 180);
+                double dLon = (lon2 - lon1) * (Math.PI / 180);
+
+                double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) + Math.Cos(lat1 * (Math.PI / 180)) * Math.Cos(lat2 * (Math.PI / 180)) * Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+
+                double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+                double distance = R * c; // Distance in km
+
+                Console.WriteLine($"The distance between {city1} and {city2} is {distance} km.");
+            }
+            catch (KeyNotFoundException)
+            {
+                Console.WriteLine("One or both of the cities are not in our data.");
+                return;
+            }
+        }
+
+        //displays the total population of a given province
+        public void DisplayProvincePopulation(string province)
+        {
+            int totalPop = 0;
+            bool hasProvince = false;
+
+
+            foreach (var city in CityCatalogue)
+            {
+                if (city.Value.GetProvince() == province)
+                {
+                    totalPop += city.Value.GetPopulation();
+                    hasProvince = true;
+                }
+            }
+
+            if (!hasProvince)
+                Console.WriteLine($"No province by the name {province} exists in our data");
+            else
+                Console.WriteLine($"The total population of {province} is: {totalPop}");
+        }
+
+        //displays all the cityies in a province
+        public void DisplayProvinceCities(string province)
+        {
+            Console.WriteLine($"Cities within {province}");
+            foreach (var city in CityCatalogue)
+            {
+                if (city.Value.GetProvince() == province)
+                {
+                    Console.WriteLine($"{city.Key}");
+                }
+            }
+        }
+
+        //gets the total population for each province, then sorts and displays by the population size
+        public void RankProvincesByPopulation()
+        {
+            Dictionary<string, int> provincePopulation = new Dictionary<string, int>();
+
+            foreach (var city in CityCatalogue)
+            {
+                if (provincePopulation.ContainsKey(city.Value.GetProvince()))
+                    provincePopulation[city.Value.GetProvince()] += city.Value.GetPopulation();
+                else
+                    provincePopulation.Add(city.Value.GetProvince(), city.Value.GetPopulation());
+            }
+            //use orderBy to put the provinces into a sorted dictionary
+            var sortedDictionary = provincePopulation.OrderBy(kvp => kvp.Value);
+
+            Console.WriteLine("Provinces by total population in ascending order:");
+            foreach (var kvp in sortedDictionary)
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
 
         }
 
+        public void RankprovincesByCities()
+        {
 
+            Dictionary<string, int> provinceCities = new Dictionary<string, int>();
 
+            foreach (var city in CityCatalogue)
+            {
+                if (provinceCities.ContainsKey(city.Value.GetProvince()))
+                    provinceCities[city.Value.GetProvince()]++;
+                else
+                    provinceCities.Add(city.Value.GetProvince(), 1);
+            }
+            //use orderBy to put the provinces into a sorted dictionary
+            var sortedDictionary = provinceCities.OrderBy(kvp => kvp.Value);
+
+            Console.WriteLine("Provinces by number of cities in ascending order:");
+            foreach (var kvp in sortedDictionary)
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+        }
+
+        public void GetCapital(string province)
+        {
+
+            //using a dictionary to have a reference to every capital
+            Dictionary<string, string> ProvinceToCapital = new Dictionary<string, string>
+            {
+                { "Alberta", "Edmonton" },
+                { "British Columbia", "Victoria" },
+                { "Manitoba", "Winnipeg" },
+                { "New Brunswick", "Fredericton" },
+                { "Newfoundland and Labrador", "St. John's" },
+                { "Nova Scotia", "Halifax" },
+                { "Ontario", "Toronto" },
+                { "Prince Edward Island", "Charlottetown" },
+                { "Quebec", "Quebec City" },
+                { "Saskatchewan", "Regina" },
+                { "Northwest Territories", "Yellowknife" },
+                { "Nunavut", "Iqaluit" },
+                { "Yukon", "Whitehorse" }
+            };
+
+            if (ProvinceToCapital.ContainsKey(province))
+            {
+                Console.WriteLine($"{province}'s capital is {ProvinceToCapital[province]} and its located at {CityCatalogue[ProvinceToCapital[province]].GetLocation()}");
+            }
+        }
 
     }
 }
